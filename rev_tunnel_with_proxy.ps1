f199fef5871d4b6185c4af102520a47b335e63a6
superpb9 <d.pippo9@gmail.com>
2023-08-02 21:51:23 +1200

    draft version

---
+++ rev_tunnel_with_proxy.ps1
@@ -0,0 +1,32 @@
+﻿# You need to download Renci.SshNet.dll from https://github.com/sshnet/SSH.NET first
+# Then, save this Renci.SshNet.dll to the same folder of this script
+# You might also need to manually right click on the DLL and unlock it before you can use it
+[void][reflection.assembly]::LoadFrom((Resolve-Path ".\Renci.SshNet.dll"))
+
+$targethost = "Remote SSH Server IP"
+$targetport = 443
+$username = "CHANGEME" # Your remote SSH server user
+$password = "CHANGEME" # Your remote SSH Server password
+
+$proxyhost = "yourproxy.host.local" # CHANGE ME
+$proxyport = 8080 # CHANGE ME
+
+$connInfo = New-Object Renci.SshNet.PasswordConnectionInfo($targetHost,$targetPort, $username, $password, "Http", $proxyhost, $proxyport)
+# You might need to use this one instead if your proxy requires authentication
+#$bytePassword = [System.Text.Encoding]::UTF8.GetBytes($password)
+#$connInfo = New-Object Renci.SshNet.PasswordConnectionInfo($targetHost,$targetPort, $username, $bytePassword, "Http", $proxyhost, $proxyport, "Proxy Auth Username (CHANGE ME)", "Proxy Auth Password (CHANGE ME)");
+
+$sshClient = New-Object Renci.SshNet.SshClient -ArgumentList $connInfo
+$sshClient.Connect()
+
+$remoteForwardedPort = New-Object Renci.SshNet.ForwardedPortRemote(
+    "127.0.0.1",   
+    1080,           # Remote (server) forwarded port 
+    "127.0.0.1",    # Local (client) SOCKS5 Listening Address
+    1080            # Local (client) SOCKS5 Listening Port
+)
+
+$sshClient.AddForwardedPort($remoteForwardedPort)
+$remoteForwardedPort.Start()
+
+    
---
+++ rev_tunnel_without_proxy.ps1
@@ -0,0 +1,24 @@
+# You need to download Renci.SshNet.dll from https://github.com/sshnet/SSH.NET first
+# Then, save this Renci.SshNet.dll to the same folder of this script
+# You might also need to manually right click on the DLL and unlock it before you can use it
+[void][reflection.assembly]::LoadFrom((Resolve-Path ".\Renci.SshNet.dll"))
+
+$targethost = "Remote SSH Server IP"
+$targetport = 443
+$username = "CHANGEME" # Your remote SSH server user
+$password = "CHANGEME" # Your remote SSH Server password
+
+$connInfo = New-Object Renci.SshNet.PasswordConnectionInfo($targethost, $targetport, $username, $password)
+
+$sshClient = New-Object Renci.SshNet.SshClient -ArgumentList $connInfo
+$sshClient.Connect()
+
+$remoteForwardedPort = New-Object Renci.SshNet.ForwardedPortRemote(
+    "127.0.0.1",   
+    1080,           # Remote (server) forwarded port 
+    "127.0.0.1",    # Local (client) SOCKS5 Listening Address
+    1080            # Local (client) SOCKS5 Listening Port
+)
+
+$sshClient.AddForwardedPort($remoteForwardedPort)
+$remoteForwardedPort.Start()
